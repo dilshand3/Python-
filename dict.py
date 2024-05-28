90c09d35748c521f81f4deb182adb10159be8f96
dilshan.d3 <dilshan72404@gmail.com>
2024-05-28 16:31:08 +0530

    this is dictionary

---
+++ dict.py
@@ -0,0 +1,11 @@
+my_dict = {
+"name": "dilshan",
+"course":"Bca",
+"age" : 17,
+"brother":["zaheer","saddam","tahir","hamid"]
+}
+
+print(my_dict)#this is for printing complete dictionary
+print(my_dict["age"])#this is for some speacial dictionary value
+print(my_dict["brother"])
+
