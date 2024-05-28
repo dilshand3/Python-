67cc7abe6c9038c9e870a806ca32925cb36b2bab
dilshan.d3 <dilshan72404@gmail.com>
2024-05-28 12:39:52 +0530

    calcultor  project in python

---
+++ input.py
@@ -0,0 +1,15 @@
+operator = input("enter a mode from '+,-,*,/' : ")
+a = int(input("enter a value : "))
+b = int(input("enter b value : "))
+
+if(operator == '+'):
+  print(a + b)
+elif(operator == '-'):
+  print(a-b)
+elif(operator == '*'):
+  print(a*b)
+elif(operator == '/'):
+  print(a/b)
+
+
+#this is example of calculator in python
