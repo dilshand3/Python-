febedfcde5e0048f23f42ddcd086c4d1fe592f9b
dilshan.d3 <dilshan72404@gmail.com>
2024-05-28 11:46:27 +0530

    number function

---
+++ number.py
@@ -0,0 +1,10 @@
+print(abs(-4))#this is for show abloute value
+print(max(3,2))#max value will print
+print(min(2,9))#min value will print
+print(round(4.4))#this will roundup value 4.4 = 4
+print(bin(2))#this will show the binary number
+
+from math import *  #this is for importing class from library math is library and * mean all import all class
+
+print(sqrt(100))#this is for square root 
+
