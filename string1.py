4436a6153495f080e052257e2825c66c34600785
dilshan.d3 <dilshan72404@gmail.com>
2024-05-28 11:33:15 +0530

    string practice

---
+++ string1.py
@@ -0,0 +1,16 @@
+print("hello \n dilshan")
+print("hello \t dilshan")
+
+print("hello","dilshan")
+print("dilshan".upper())#these are built in function
+print("DILSHAN".lower())
+print("DILSHAN"[6])
+
+print("DILSHAN".islower())
+print("DILSHAN".isupper())
+print("dilshan".lower().isupper())
+print("dilshan".lower().islower())
+print("dilshan".__len__())#this is used for leangth of string 
+print(len("dilshan"))#this is also used for leangth of string 
+print("dilshan".index("i"))#this is for knowing the where is are character
+print("dilshan".replace("n","d"))#this is for replacing our character
