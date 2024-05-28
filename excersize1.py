53d1d37b2b1d9e390e14331b8ed9c900c7c60dd0
dilshan.d3 <dilshan72404@gmail.com>
2024-05-28 13:30:16 +0530

    word replacing programe

---
+++ excersize1.py
@@ -0,0 +1,8 @@
+sentence = input("enter a sentence : ") 
+print("your sentence is :",sentence)
+oldword = input("enter the old word : ")
+newword = input("enter the new word : ")
+print(sentence.replace(oldword,newword))
+
+
+#this is a programe for replacing the word in string 
