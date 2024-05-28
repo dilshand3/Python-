d57465c43b47667b533edc80452ca5b318028a65
dilshan.d3 <dilshan72404@gmail.com>
2024-05-28 11:09:17 +0530

    operator

--- two.py
+++ two.py
@@ -1,4 +1,20 @@
-a = 2
-b = 3
+#These are arithmetical operator in python 
+a = 4 #varaibale declration
+print(2+4)    #sum operator
+print(2-4)    #sub operator 
+print(4*2)    #multiply operator
+print(8/2)    #devide operator
+print(9%2)    #modules operator
 
-print(a+b)
+
+#these are relational operator
+print(2 == 4)#equal operator
+print(2>1)#greater then operator
+print(2<3)#less then operator 
+print(2!=3)#not equal to operator
+print(2>=2)#greater then equal operator
+print(2<=3)#less then equal operator
+
+#these are logical operator
+# && = this and operator
+# || = this or operator
