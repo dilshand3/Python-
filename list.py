d64d3aea8d4045f8fd801e229d52546cfc9c6242
dilshan.d3 <dilshan72404@gmail.com>
2024-05-28 13:59:30 +0530

    list in python

---
+++ list.py
@@ -0,0 +1,15 @@
+coutries = ["india","qatar","palestine","saudi-arbia"]
+print(coutries[1])
+print(coutries)
+print(type(coutries))
+coutries[1] = "bahrin"#we can replace it like this 
+print(coutries)
+
+#second type of list in python. Both are same and we can write many type of value in list 
+
+city = list(('new-york','amsterdan','dubai','delhi',False,34))
+print(city)
+print(type(coutries))
+print(type(city[4]))
+print(type(city[0]))
+print(type(city[5]))
