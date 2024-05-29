cf814fde19e2b86f3b5fcce6374e607cc455700e
dilshan.d3 <dilshan72404@gmail.com>
2024-05-29 11:13:36 +0530

    return keyword in python

---
+++ return.py
@@ -0,0 +1,9 @@
+def sum(a,b):
+    return a+b
+
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+print(sum(a,b))
+
+
+#this is the sum funtion programe from user input for sum
