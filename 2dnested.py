572b60ada37479cd5a59e731ee3756769578ece3
dilshan.d3 <dilshan72404@gmail.com>
2024-05-29 11:46:18 +0530

    2d array with nested loop

---
+++ 2dnested.py
@@ -0,0 +1,7 @@
+dilshan = [ [1,2,3,4], [5,6,7,8], [9,0]]
+
+print(dilshan)#this will print array with bracket
+
+for value in dilshan:#this will print only value
+    for val in value:
+     print(val)
