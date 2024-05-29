eb18f4a51222469191307118683e1eb2a13ab91b
dilshan.d3 <dilshan72404@gmail.com>
2024-05-29 11:54:50 +0530

    try and except in python

---
+++ try,except.py
@@ -0,0 +1,12 @@
+try:
+    a = int(input("enter a value : "))
+    print(a)
+except:
+    print("the value is not an interger")
+else:#we can use alse also if try statement is true then the elsle statement will print
+    print("Nothing went wrong ")
+finally:#we can use also finally. it always print . we use it without else and else without finally 
+    print("this is programe of try and except ")
+
+
+    #please run this is programe you will understand it
